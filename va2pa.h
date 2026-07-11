0c4d9e527471b4824b6d524c562e32b88e30e833
Element <414280563@qq.com>
2026-07-11 22:07:02 +0800

    update va2pa

--- va2pa.h
+++ va2pa.h
@@ -5,12 +5,37 @@ inline PVOID Va2Pa(PVOID VirtualAddress);
 
 inline PVOID Va2Pa(PVOID VirtualAddress)
 {
-	auto res = spf::memory_map::current();
+	static spf::memory_map g_cached_map;
+	static bool g_initialized = false;
 
+	if (!g_initialized)
+	{
+		auto res = spf::memory_map::current();
+		if (res.has_value())
+		{
+			g_cached_map = std::move(res.value());
+			g_initialized = true;
+		}
+		else
+		{
+			return nullptr;
+		}
+	}
+
+	std::uint64_t pa = g_cached_map.translate(VirtualAddress);
+	if (pa != 0)
+	{
+		return reinterpret_cast<PVOID>(pa);
+	}
+
+	auto res = spf::memory_map::current();
 	if (res.has_value())
 	{
-		auto mm = res.value();
-		return reinterpret_cast<PVOID>(mm.translate(VirtualAddress));
+		g_cached_map = std::move(res.value());
+
+		pa = g_cached_map.translate(VirtualAddress);
+		return reinterpret_cast<PVOID>(pa);
 	}
-	return 0;
+
+	return nullptr;
 }
